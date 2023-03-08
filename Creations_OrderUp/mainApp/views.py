114e60a4e44e25fc80133383ad21818161bb617e
AdamSalonen1 <adam.salonen@gmail.com>
2023-03-08 08:34:07 -0600

    Connected template to model in very simple fashion

--- Creations_OrderUp/mainApp/views.py
+++ Creations_OrderUp/mainApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mainApp.models import Order
 
 def index(request):
-    return render(request, 'mainApp/index.html')
+    full_order_list = Order.objects.order_by('orderId')
+    full_order_dict = {'Order': full_order_list}
+    return render(request, 'mainApp/index.html', context = full_order_dict)
 
 def advertisements(request):
     return render(request, 'mainApp/advertisements.html')
