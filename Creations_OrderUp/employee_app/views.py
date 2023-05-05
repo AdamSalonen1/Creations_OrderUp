f288dbcddea1037d5a6ea1e3e038f3cb8da966f5
AdamSalonen1 <102756507+AdamSalonen1@users.noreply.github.com>
2023-05-05 17:15:27 -0500

    some functionality where the employee should post to main but for some reason it doesn't and it makes no sense

--- Creations_OrderUp/employee_app/views.py
+++ Creations_OrderUp/employee_app/views.py
@@ -20,15 +20,15 @@ def index(request):
 
         form = MealForm(initial={'meal_name' : lastMeal})
 
-        full_order_list = Order.objects.order_by('order_id')
+        full_order_list = Order.objects.filter(is_complete = False)
         meal_list = Meal.objects.order_by('meal_name')
-        if request.method == 'POST':  
+        if request.method == 'POST': 
             selectedMeal = Meal.objects.get(meal_name=request.POST.get('meal_name').__str__())
             now = datetime.now()
             order=Order()
             order.order_number = request.POST.get('number')
-            order.start_time = now.strftime("%Y-%m-%d %H:%M")
-            order.est_end_time = (now + timedelta(seconds=selectedMeal.cook_time)).strftime("%Y-%m-%d %H:%M")
+            order.start_time = now.strftime("%Y-%m-%d %H:%M:%S")
+            order.est_end_time = (now + timedelta(seconds=selectedMeal.cook_time)).strftime("%Y-%m-%d %H:%M:%S")
             order.meal = selectedMeal
             order.save()
 
@@ -37,5 +37,6 @@ def index(request):
     except:
         return HttpResponseRedirect('/employee?hasError=True')
 
-    return render(request, 'employee_app/employee.html', context = {'Order': full_order_list, 'Meal': meal_list, 'form':form, 'hasError':hasError})
+    return render(request, 'employee_app/employee.html', context = {'Order': full_order_list, 'Meal': meal_list, 'form':form, 
+                                                                    'hasError':hasError})
 
