4652a29f7249e1673d6fe8dd304719de0ba9517a
AdamSalonen1 <102756507+AdamSalonen1@users.noreply.github.com>
2023-05-04 22:22:56 -0500

    orders are removed from progress bar at time of completion

--- Creations_OrderUp/mainApp/views.py
+++ Creations_OrderUp/mainApp/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
 from employee_app.models import Order
 import json
 
 def index(request):
-    full_order_list = Order.objects.order_by('order_id')
-    full_order_dict = {'Order': full_order_list}
-    if request.method == 'POST':
+    full_order_list = Order.objects.filter(is_complete = False)
+
+    if request.method == 'POST':      
         json_data = json.loads(request.body.decode('utf-8'))
         order = Order.objects.get(order_id = json_data['order_id'])
         
-        print(json_data['order_id'])
-        order
         order.is_complete = True
         print(order.est_end_time)
         order.save()
-        
-    return render(request, 'mainApp/index.html', context = full_order_dict)
+        order.save_base()
+
+        full_order_list = Order.objects.filter(is_complete = False)
+
+        return render(request, 'mainApp/index.html', context = {'Order':full_order_list})
+    
+           
+    return render(request, 'mainApp/index.html', context = {'Order':full_order_list})
 
 
 def advertisements(request):
