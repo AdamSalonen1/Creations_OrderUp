53cb4219049f7111fc8e3fd79baa04e50dfbf886
Jack Lyrek <jacklyrek@gmail.com>
2023-05-05 23:24:11 -0500

    adverts

--- Creations_OrderUp/mainApp/views.py
+++ Creations_OrderUp/mainApp/views.py
@@ -4,28 +4,28 @@ from django.http import HttpResponseRedirect
 
 from employee_app.models import Order
 import json
+import os
+
 
 def index(request):
-    full_order_list = Order.objects.filter(is_complete = False)
+    full_order_list = Order.objects.filter(is_complete=False)
 
-    if request.method == 'POST':    
-        print("posting")  
+    if request.method == 'POST':
+        print("posting")
         json_data = json.loads(request.body.decode('utf-8'))
-        order = Order.objects.get(order_id = json_data['order_id'])
-        
+        order = Order.objects.get(order_id=json_data['order_id'])
+
         order.is_complete = True
         order.save(force_update=True)
 
-        full_order_list = Order.objects.filter(is_complete = False)
+        full_order_list = Order.objects.filter(is_complete=False)
 
         print('line before httpresponse redirect')
-        return render(request, 'mainApp/index.html', context = {'Order':full_order_list})
-    
+        return render(request, 'mainApp/index.html', context={'Order': full_order_list})
 
-    return render(request, 'mainApp/index.html', context = {'Order':full_order_list})
-    
-           
+    return render(request, 'mainApp/index.html', context={'Order': full_order_list})
+
 
 def advertisements(request):
-    full_order_list = Order.objects.filter(is_complete = False)
-    return render(request, 'mainApp/advertisements.html', context = {'Order':full_order_list})
+    full_order_list = Order.objects.filter(is_complete=False)
+    return render(request, 'mainApp/advertisements.html', context={'Order': full_order_list})
