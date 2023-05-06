28f8f407baf89bc74d0d2d8d9ce1729d3e4774b8
Jack Martinka <91498376+Venkal@users.noreply.github.com>
2023-05-05 22:13:12 -0500

    Complete Order

--- Creations_OrderUp/employee_app/views.py
+++ Creations_OrderUp/employee_app/views.py
@@ -22,18 +22,30 @@ def index(request):
 
         full_order_list = Order.objects.filter(is_complete = False)
         meal_list = Meal.objects.order_by('meal_name')
-        if request.method == 'POST': 
-            selectedMeal = Meal.objects.get(meal_name=request.POST.get('meal_name').__str__())
-            now = datetime.now()
-            order=Order()
-            order.order_number = request.POST.get('number')
-            order.start_time = now.strftime("%Y-%m-%d %H:%M:%S")
-            order.est_end_time = (now + timedelta(seconds=selectedMeal.cook_time)).strftime("%Y-%m-%d %H:%M:%S")
-            order.meal = selectedMeal
-            order.save()
 
-            return HttpResponseRedirect('/employee?lastMeal={0}'.format(request.POST.get('meal_name').__str__()))
-        
+        if request.method == 'POST': 
+            print(request.POST.get('orders'))
+            if request.POST.get('Complete')=='Complete':
+                print("thank god")
+                complete_Order = Order.objects.get(order_number = request.POST.get('orders'))
+                print("got complete order")
+                complete_Order.is_complete = True
+                print("iscomplete = true")
+                complete_Order.save()
+                print("order saved")
+                return HttpResponseRedirect('/employee?lastMeal={0}'.format(request.POST.get('meal').__str__()))
+            
+            else:
+                print("NOOOOOOO")
+                selectedMeal = Meal.objects.get(meal_name=request.POST.get('meal_name').__str__())
+                now = datetime.now()
+                order=Order()
+                order.order_number = request.POST.get('number')
+                order.start_time = now.strftime("%Y-%m-%d %H:%M:%S")
+                order.est_end_time = (now + timedelta(seconds=selectedMeal.cook_time)).strftime("%Y-%m-%d %H:%M:%S")
+                order.meal = selectedMeal
+                order.save()
+                return HttpResponseRedirect('/employee?lastMeal={0}'.format(request.POST.get('meal_name').__str__()))
     except:
         return HttpResponseRedirect('/employee?hasError=True')
 
